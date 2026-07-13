0a587b51b383013cc2c61e9c0bd2ace4b279f75d
unknown <ahaseko1@gmail.com>
2026-07-13 10:34:03 +0700

    fix build

--- hooks/posts/usePosts.ts
+++ hooks/posts/usePosts.ts
@@ -1,14 +1,14 @@
 // hooks/posts/usePosts.ts
 import useSWR from "swr";
-import { getPosts } from "@/lib/api/posts.api";
+// import { getPosts } from "@/lib/api/posts.api";
 
-export function usePosts() {
-  const { data, error, isLoading, mutate } = useSWR("/api/posts", getPosts);
+// export function usePosts() {
+//   const { data, error, isLoading, mutate } = useSWR("/api/posts", getPosts);
 
-  return {
-    posts: data ?? [],
-    isLoading,
-    isError: error,
-    mutate,
-  };
-}
+//   return {
+//     posts: data ?? [],
+//     isLoading,
+//     isError: error,
+//     mutate,
+//   };
+// }
