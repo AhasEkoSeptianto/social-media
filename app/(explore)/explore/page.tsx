85d9444ba30f0e942a0c7b32063be0987db15e99
unknown <ahaseko1@gmail.com>
2026-07-16 11:49:57 +0700

    search user

--- app/(explore)/explore/page.tsx
+++ app/(explore)/explore/page.tsx
@@ -2,13 +2,19 @@
 
 import NavigationMenuMobile from "@/components/shared/NavigationMenuMobile";
 import { AppSidebar } from "@/components/shared/Sidebar";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { Field } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
 import { SidebarProvider } from "@/components/ui/sidebar";
+import { Skeleton } from "@/components/ui/skeleton";
+import { useUser } from "@/hooks/auths/useUser";
+import { searchUsers } from "@/lib/api/users.api";
+import { nextFetcher } from "@/lib/fetcher";
 import { Play } from "lucide-react";
 import Image from "next/image";
-import { useState } from "react";
+import { Fragment, useEffect, useState } from "react";
+import useSWR from "swr";
 
 const tagMenu = [
   "All",
@@ -22,27 +28,18 @@ const tagMenu = [
   "Tech",
 ];
 
-interface ExploreItem {
-  id: string;
-  src: string;
-  alt: string;
-  isVideo?: boolean;
-  span?: "tall"; // buat item yang row-span-2
-}
-
-const items: ExploreItem[] = [
-  { id: "1", src: "/images/post1.avif", alt: "Mountain sunset", span: "tall" },
-  { id: "2", src: "/images/post2.avif", alt: "Green sofa" },
-  { id: "3", src: "/images/post3.avif", alt: "Restaurant dish", isVideo: true },
-  { id: "4", src: "/images/post4.avif", alt: "Dog closeup" },
-  { id: "5", src: "/images/post5.avif", alt: "Eiffel tower", isVideo: true },
-  { id: "6", src: "/images/post6.avif", alt: "Pool float" },
-  { id: "7", src: "/images/post7.avif", alt: "Ocean waves" },
-  { id: "8", src: "/images/post8.avif", alt: "Man portrait" },
-];
-
 export default function ExplorePage() {
   const [selectedTag, setSelectedTag] = useState("All");
+  const [search, setSearch] = useState("");
+  const { user: myUser } = useUser();
+  const { data: users, isLoading } = useSWR(
+    `/api/users?search=${search}`,
+    searchUsers,
+    {
+      shouldRetryOnError: false,
+    },
+  );
+
   return (
     <SidebarProvider className="">
       <div className="hidden lg:block">
@@ -56,12 +53,18 @@ export default function ExplorePage() {
         <div className="col-span-9 lg:container space-y-8">
           <div className="lg:flex items-center justify-between space-y-4 lg:space-y-0">
             <Field orientation="horizontal" className="bg-brand w-full lg:w-60">
-              <Input type="search" placeholder="Search people,tag,place" />
+              <Input
+                type="search"
+                placeholder="Search people,tag,place"
+                onChange={(e) => setSearch(e.target.value)}
+              />
               <Button variant="secondary" className="bg-brand5">
                 Search
               </Button>
             </Field>
-            <div className="flex flex-wrap items-center space-x-2">
+            <div
+              className={`flex flex-wrap items-center space-x-2 ${search ? "hidden" : ""}`}
+            >
               {tagMenu.map((tag, idx) => (
                 <Button
                   size="lg"
@@ -76,32 +79,91 @@ export default function ExplorePage() {
             </div>
           </div>
 
-          <div className="grid grid-cols-3 gap-2 auto-rows-[200px]">
-            {items.map((item) => (
-              <div
-                key={item.id}
-                className={`relative overflow-hidden rounded-lg bg-neutral-900 ${
-                  item.span === "tall" ? "row-span-2" : ""
-                }`}
-              >
-                <Image
-                  src={item.src}
-                  fill
-                  className="object-cover"
-                  alt={item.alt}
-                  sizes="(max-width: 768px) 33vw, 300px"
-                  loading="eager"
-                />
-                {item.isVideo && (
-                  <div className="absolute right-2 top-2 flex h-6 w-6 items-center justify-center rounded-full bg-black/50">
-                    <Play className="h-3 w-3 fill-white text-white" />
-                  </div>
-                )}
-              </div>
-            ))}
-          </div>
+          {search ? (
+            <div className="space-y-2">
+              {isLoading
+                ? Array.from({ length: 8 }).map((i, idx) => (
+                    <Skeleton className="h-10 w-full" key={idx} />
+                  ))
+                : users?.data?.map((user: any, idx: number) => (
+                    <div
+                      className={`flex items-center justify-between hover:bg-brand4 rounded-lg p-2 ${user?._id === myUser?.id ? "hidden" : ""}`}
+                      key={idx}
+                    >
+                      <div className="flex items-center gap-2 ">
+                        <Avatar size="lg">
+                          <AvatarImage src={user?.avatar_url} alt="@shadcn" />
+                          <AvatarFallback>CN</AvatarFallback>
+                        </Avatar>
+                        <div>
+                          <p>{user?.name}</p>
+                          <p>{user?.username}</p>
+                        </div>
+                      </div>
+                      <Button className="bg-brand5 text-white">Follow</Button>
+                    </div>
+                  ))}
+            </div>
+          ) : (
+            <PreviewPosts />
+          )}
         </div>
       </main>
     </SidebarProvider>
   );
 }
+
+const PreviewPosts = () => {
+  const [postList, setPostList] = useState([]);
+  const [loading, setLoading] = useState({ getPosts: true });
+  const getPosts = async () => {
+    setLoading((prev) => ({ ...prev, getPosts: true }));
+    const myPost = await nextFetcher(`/api/posts/get?images=true`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then((resp) => {
+      setPostList(resp?.data?.posts);
+    });
+
+    setLoading((prev) => ({ ...prev, getPosts: false }));
+  };
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+
+  return (
+    <div className="grid grid-cols-3 gap-2 auto-rows-[200px]">
+      {loading.getPosts ? (
+        <Fragment>
+          {Array.from({ length: 3 }).map((i: any, idx: number) => (
+            <Skeleton key={idx} className="h-60 w-full" />
+          ))}
+        </Fragment>
+      ) : (
+        postList?.map((item: any, idx) => (
+          <div
+            key={item._id}
+            className={`relative overflow-hidden rounded-lg bg-neutral-900 `}
+          >
+            <Image
+              src={item.images}
+              fill
+              className="object-cover"
+              alt={`image ${idx}`}
+              sizes="(max-width: 768px) 33vw, 300px"
+              loading="eager"
+            />
+            {/* {item.isVideo && (
+                  <div className="absolute right-2 top-2 flex h-6 w-6 items-center justify-center rounded-full bg-black/50">
+                    <Play className="h-3 w-3 fill-white text-white" />
+                  </div>
+                )} */}
+          </div>
+        ))
+      )}
+    </div>
+  );
+};
